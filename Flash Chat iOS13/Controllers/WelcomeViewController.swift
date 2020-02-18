98e90e8b1f4661fef841410435c034bccab79a47
mdave94 <molnardavid2002@gmail.com>
2020-02-18 21:40:37 +0100

    importing cocoapods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,27 +7,17 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        titleLabel.text = ""
-        let titleText = "ChatApp"
-        var index = 0.0
-        
-        
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1*index, repeats: false) { (timer) in
-                
-                self.titleLabel.text?.append(letter)
-                    }
-            index+=1
-        }
-        
+   
+        titleLabel.text = "ChatApp"
         
         
        
